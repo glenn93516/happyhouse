1991ca8671eb85dd24cfc7e7cf1b157aae98983d
AhreumLee <49761664+lahreum@users.noreply.github.com>
2020-11-25 13:48:00 +0900

    hoho

--- client/src/views/Main.vue
+++ client/src/views/Main.vue
@@ -1,28 +1,29 @@
 <template>
-    <div id="home" class="d-flex justify-content-center align-items-center">
-        <span>UNHAPPY</span>
-    </div>
+  <div id="home" class="d-flex justify-content-center align-items-center">
+    <span class="logotitle">UNHAPPY, <br />BUT HAPPY</span>
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-
-        };
-    }
-}
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
 #home {
-    background: url('../assets/building.png') no-repeat center;
-    background-size: 50% 50%;
-    font-family: 'Nanum Gothic', sans-serif;
-    font-size: 120px;
-    font-weight: 1000;
-    height: 60vh;
-    /* color: #aff1dd; */
-    color : white;
+  background: url('../assets/building.png') no-repeat center;
+  background-size: 50% 50%;
+  font-family: 'Nanum Gothic', sans-serif;
+  font-size: 120px;
+  font-weight: 1000;
+  height: 60vh;
+  /* color: #aff1dd; */
+  color: white;
+}
+.logotitle {
+  line-height: 100%;
 }
 </style>
